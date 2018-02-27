65e3088560cea375df172fb0722d5641082d06ff
abedbilani <abed.bilani@gmail.com>
2018-02-28 00:36:07 +0200

    prise en main

---
+++ src/classbuilder/ClassBuilder.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classbuilder;
+
+/**
+ *
+ * @author Abed Bilani
+ */
+public class ClassBuilder {
+    public static void main(String[] args) {
+        // to be added
+    }
+}
---
+++ src/classbuilder/Client.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classbuilder;
+
+/**
+ *
+ * @author Abed Bilani
+ */
+public class Client {
+    private String id;
+    private String prenom;
+    private String nom;
+    private String telephone;
+    private String rue;
+    private String ville;
+    private String etat;
+    private String code;
+    private String pays;
+    private String mail;
+}
